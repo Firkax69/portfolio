86b72950dcca55cee29b3580bb9b65c7a40e6b35
firkax <157710697+Firkax69@users.noreply.github.com>
2024-10-07 15:01:04 +0100

    Add page transition animation

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,90 @@
 import "./App.css";
-import {Outlet, RouterProvider, createBrowserRouter} from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  useLocation,
+} from "react-router-dom";
+import { AnimatePresence, motion } from "framer-motion";
 
 import Header from "./components/Header";
 import Home from "./pages/Home";
 import Projects from "./pages/Projects";
 
+const pageVariants = {
+  initial: {
+    opacity: 0,
+    x: "-100vw",
+  },
+  in: {
+    opacity: 1,
+    x: 0,
+  },
+  out: {
+    opacity: 0,
+    x: "100vw",
+  },
+};
+
+const pageTransition = {
+  type: "tween",
+  ease: "easeInOut",
+  duration: 0.5,
+};
+
 const Layout = () => {
+  const location = useLocation();
+
   return (
     <>
       <Header/>
       <div id="app-main-content-container">
-        <Outlet/>
+        <AnimatePresence mode="wait">
+          <Routes location={location} key={location.pathname}>
+            <Route
+              path="/"
+              element={
+                <motion.div
+                  initial="initial"
+                  animate="in"
+                  exit="out"
+                  variants={pageVariants}
+                  transition={pageTransition}
+                >
+                  <Home/>
+                </motion.div>
+              }
+            />
+
+            <Route
+              path="projects"
+              element={
+                <motion.div
+                  initial="initial"
+                  animate="in"
+                  exit="out"
+                  variants={pageVariants}
+                  transition={pageTransition}
+                >
+                  <Projects/>
+                </motion.div>
+              }
+            />
+          </Routes>
+        </AnimatePresence>
       </div>
     </>
   );
 };
 
 function App() {
-  //Router
-
-  const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <Layout/>,
-      children: [
-        {index: true, element: <Home/>},
-        {path: "projects", element: <Projects />},
-      ],
-    }
-  ])
-
-  return <RouterProvider router={router} />;
+  return (
+    <Router>
+      <Routes>
+        <Route path="*" element={<Layout/>} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
