39c9069303baa82cbb7153acee781a1be1fbedd5
firkax <157710697+Firkax69@users.noreply.github.com>
2024-10-07 12:42:26 +0100

    create: project page

---
+++ src/pages/Projects.jsx
@@ -0,0 +1,52 @@
+import ProjectShowcase from "../components/ProjectShowcase";
+import { Link } from "react-router-dom";
+import {featuredProjects, projects} from "../data/projects";
+import styles from "./Projects.module.css";
+
+const notFeaturedProjects = projects.filter((project) => 
+    !project.featured);
+
+const Banner = () => {
+    return (
+        <div className={styles["banner"]}>
+            <h1>Projects</h1>
+            <h2>Here's some of the work I've done so far.</h2>
+        </div>
+    );
+};
+
+const TableOfContents = ({projects}) => {
+    return (
+        <div className={styles["table-of-contents"]}>
+            <h3>Table of Contents</h3>
+            <ul>
+                {projects.map((project) => (
+                    <li key={project.name}>
+                        <Link to={project.name} smooth={true} duration={500} offset={-30}>
+                            {project.title}
+                        </Link>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    )
+}
+
+const Projects = () => {
+    const orderedProjects = [...featuredProjects, notFeaturedProjects];
+
+    return (
+        <>
+            <Banner/>
+            <hr />
+            <TableOfContents projects={orderedProjects}/>
+            <hr />
+            <ProjectShowcase sectionTitle="Pinned" projects={featuredProjects}/>
+            <hr />
+            <ProjectShowcase sectionTitle="Recent" projects={notFeaturedProjects}/>
+            <hr />
+        </>
+    )
+}
+
+export default Projects;
