f8f1b8ca33c0a5d8aaf5c7cfdf45caa01a0f8e0e
firkax <157710697+Firkax69@users.noreply.github.com>
2024-10-07 11:53:55 +0100

    feat: set up router

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,36 @@
 import "./App.css";
+import {Outlet, RouterProvider, createBrowserRouter} from "react-router-dom";
 
-function App() {
+import Header from "./components/Header";
+// import Home from "./pages/Home";
+// import Projects from "./pages/Projects";
+
+const Layout = () => {
   return (
     <>
-      <h1>Hello, and welcome.</h1>
+      <Header/>
+      <div id="app-main-content-container">
+        <Outlet/>
+      </div>
     </>
-  )
+  );
+};
+
+function App() {
+  //Router
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout/>,
+      children: [
+        {index: true, element: <Home/>},
+        {path: "projects", element: <Projects />},
+      ],
+    }
+  ])
+
+  return <RouterProvider router={router} />;
 }
 
 export default App;
--- src/components/ProjectShowcase.jsx
+++ src/components/ProjectShowcase.jsx
@@ -1,13 +1,13 @@
 import styles from "./ProjectShowcase.module.css";
 import ProjectCard from "./ProjectCard.jsx";
-// import projects from "../data/projects.js"
+import { featuredProjects } from "../data/projects.js";
 
 const ProjectShowcase = ({sectionTitle, projects}) => {
     return (
         <div className={styles["section-container"]}>
             <h2>{sectionTitle}</h2>
             <div className={styles["projects-container"]}>
-                {projects.map((project) => (
+                {featuredProjects.map((project) => (
                     <ProjectCard key={project.title} {...project} />
                 ))}
             </div>
