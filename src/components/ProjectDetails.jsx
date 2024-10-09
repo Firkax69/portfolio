863538382e9a3f56feac9c48cf1e92ee991c163b
firkax <157710697+Firkax69@users.noreply.github.com>
2024-10-09 08:49:32 +0100

    create: PorjectDetails page

---
+++ src/components/ProjectDetails.jsx
@@ -0,0 +1,45 @@
+import { useParams } from "react-router-dom";
+import projects from "../data/projects";
+import ProjectLinks from "../components/ProjectLinks";
+import styles from "./ProjectDetails.module.css";
+
+const ProjectDetails = () => {
+    //  ????????? this two lines
+    const {projectName} = useParams();
+    const project = projects.find((proj) => proj.name === projectName);
+
+    return (
+        <div className={styles["project-content-container"]}>
+            <div className={styles["project-info-container"]}>
+                <div className={styles["header-group"]}>
+                    <span>Project Details</span>
+                    <h3 className={styles.title}>{project.title}</h3>
+                </div>
+
+                <p className={styles["description-long"]}>{project.description_long}</p>
+                <h4 className={styles["features-header"]}>Features</h4>
+                <ul className={styles["features-list"]}>
+                    {project.features.map((feature) => (
+                        <li key={feature}>{feature}</li>
+                    ))}
+                </ul>
+                <div className={styles["tech-stack-container"]}>
+                    <h4>Tech Stack</h4>
+                    <span>
+                        {projects.tech.reduce((acc, curr, idx) => {
+                            return (
+                                acc + " " + curr + (idx < project.tech.length - 1 ? "," : "")
+                            )
+                        }, "")}
+                    </span>
+                </div>
+
+                <ProjectLinks {...project}/>
+            </div>
+
+            <img src={project.img} alt="" className={styles.screenshot}/>
+        </div>
+    )
+}
+
+export default ProjectDetails;
--- src/components/ProjectLinks.jsx
+++ src/components/ProjectLinks.jsx
@@ -3,7 +3,7 @@ import styles from "./ProjectLinks.module.css";
 
 const ProjectLinks = ({live_link, repo_link}) => {
     return (
-        <div className="styles.buttons">
+        <div className={styles.buttons}>
             <a href={live_link}>Live Preview</a>
             <a href={repo_link}>Github Repo</a>
         </div>
